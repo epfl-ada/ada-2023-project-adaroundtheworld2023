e9966c52446fae78b22446c102bed03d02f4c78a
Jan Kokla <jan.kokla@gmail.com>
2023-11-11 16:21:00 +0100

    update preprocess

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,11 +1,7 @@
 import json
 
 
-def get_list_of_genres(value: str) -> list:
+def get_list_from_string_dict(value: str) -> list:
     """Parse dict string and return the list of strings with genres."""
     return list(json.loads(value).values())
 
-
-def get_str_of_genres(value: str) -> str:
-    """Parse dict string and return only genres as joint lower-case string separated by ','."""
-    return ','.join(get_list_of_genres(value)).lower()
