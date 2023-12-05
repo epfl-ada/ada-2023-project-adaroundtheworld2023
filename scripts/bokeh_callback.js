4aab2470b7290d96fd3851d95c4bb7f3985e7b81
Jan Kokla <jan.kokla@gmail.com>
2023-12-05 20:53:43 +0100

    add graph to plot function (halfway there)

--- scripts/bokeh_callback.js
+++ scripts/bokeh_callback.js
@@ -1,23 +1,17 @@
 const nodes = node_dict;
 const edges = edges_dict;
-let selected_release_year = cb_obj.value;
-
-if (selected_release_year === undefined) {
-    selected_release_year = Math.min(... nodes.release_year);
-}
+let selected_release_years = cb_obj.value;
 
 // Update the nodes source with nodes filtered by the selected release date
 
 const indices = [];
 const wikipediaIds = [];
 for (let i = 0; i < nodes['release_year'].length; i++) {
-    if (nodes['release_year'][i] === selected_release_year) {
+    if (selected_release_years.includes(nodes['release_year'][i].toString())) {
         indices.push(i);
         wikipediaIds.push(nodes['wikipedia_id'][i]);
     }
 }
-
-debugger;
 
 const newNodes = Object.keys(nodes).reduce((result, key) => {
     result[key] = [];
@@ -37,9 +31,6 @@ Object.keys(edges).forEach(key => {
         wikipediaIds.includes(edges['end'][index])
     );
 });
-
-console.log(indices)
-console.log(filteredEdges['start'])
 
 // Trigger update
 graph.node_renderer.data_source.data = newNodes;
